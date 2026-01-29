8567cf93d7db4b2cea9cdb8680a2c1eb000c6b00
Jayden Currence <64615434+NerdForEternity@users.noreply.github.com>
2026-01-29 11:45:03 -0500

    Incomplete Task 3
    Was informed it was not necessary for submission

--- Assets/Scripts/Task_3.cs
+++ Assets/Scripts/Task_3.cs
@@ -2,10 +2,21 @@ using UnityEngine;
 
 public class Task_3 : MonoBehaviour
 {
+    [SerializeField] private float playerServeScoreChance;
+    [SerializeField] private float playerRecentWinScoreChance;
+    [SerializeField] private float playerRecentLoseScoreChance;
+
+    private int currentPlayerScore;
+    private int currentOpponentScore;
+    private bool isMatchPoint;
+
+
+
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        Debug.Log("Task 3: (INCOMPLETE) Estimate chance of player winning a tennis match, based on previous history.");
     }
 
     // Update is called once per frame
