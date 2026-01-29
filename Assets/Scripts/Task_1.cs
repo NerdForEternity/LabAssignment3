c7e4153eb1b4a2f5bac114f22f9ec3bd922a44a8
Jayden Currence <64615434+NerdForEternity@users.noreply.github.com>
2026-01-29 11:27:40 -0500

    Task 1 Complete

--- Assets/Scripts/Task_1.cs
+++ Assets/Scripts/Task_1.cs
@@ -5,13 +5,28 @@ public class Task_1 : MonoBehaviour
 {
     [SerializeField] private int expectedPayment;
     private int remainingPayment;
+    private int hundredsCount;
+    private int fiftiesCount;
+    private int twentiesCount;
+    private int tensCount;
+    private int fivesCount;
+    private int onesCount;
+
 
 
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        Debug.Log("Task 1: Calculate the number of bills needed for a given amount of money.");
+        Debug.Log("Expected Payment: " + expectedPayment);
+        remainingPayment = expectedPayment;
+        CalculateHundreds(remainingPayment);
+        CalculateFifties(remainingPayment);
+        CalculateTwenties(remainingPayment);
+        CalculateTens(remainingPayment);
+        CalculateFives(remainingPayment);   
+        CalculateOnes(remainingPayment);
     }
 
     // Update is called once per frame
@@ -20,10 +35,75 @@ public class Task_1 : MonoBehaviour
         
     }
 
-    int CalculateHundreds(int change)
+    void CalculateHundreds(int change)
     {
-        private int remainingChange = 
+        while (change >= 100)
+        {
+            remainingPayment -= 100;
+            change -= 100;
+            hundredsCount++;
+            
+        }
+        Debug.Log("Hundreds: " + hundredsCount);
     }
 
+    void CalculateFifties(int change)
+    {
+        while (change >= 50)
+        {
+            remainingPayment -= 50;
+            change -= 50;
+            fiftiesCount++;
+            
+        }
+        Debug.Log("Fifties: " + fiftiesCount);
+    }
 
+    void CalculateTwenties(int change)
+    {
+        while (change >= 20)
+        {
+            remainingPayment -= 20;
+            change -= 20;
+            twentiesCount++;
+            
+        }
+        Debug.Log("Twenties: " + twentiesCount);
+    }
+
+    void CalculateTens(int change)
+    {
+        while (change >= 10)
+        {
+            remainingPayment -= 10;
+            change -= 10;
+            tensCount++;
+            
+        }
+        Debug.Log("Tens: " + tensCount);
+    }
+
+    void CalculateFives(int change)
+    {
+        while (change >= 5)
+        {
+            remainingPayment -= 5;
+            change -= 5;
+            fivesCount++;
+            
+        }
+        Debug.Log("Fives: " + fivesCount);
+    }
+
+    void CalculateOnes(int change)
+    {
+        while (change >= 1)
+        {
+            remainingPayment -= 1;
+            change -= 1;
+            onesCount++;
+            
+        }
+        Debug.Log("Ones: " + onesCount);
+    }
 }
