94dd9885e17d7f97cc6c9fdf60d93921e7e7d550
Jayden Currence <64615434+NerdForEternity@users.noreply.github.com>
2026-01-29 10:56:52 -0500

    init

---
+++ Assets/Scripts/Task_1.cs
@@ -0,0 +1,29 @@
+using System.Runtime.CompilerServices;
+using UnityEngine;
+
+public class Task_1 : MonoBehaviour
+{
+    [SerializeField] private int expectedPayment;
+    private int remainingPayment;
+
+
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    int CalculateHundreds(int change)
+    {
+        private int remainingChange = 
+    }
+
+
+}
